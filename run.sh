3cadf3e789aa42c2a54f56285442fb6b204b2e7c
sfdafaa <148449382+sfdafaa@users.noreply.github.com>
2023-10-19 22:08:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+pip3 install -y snapd
+
+snap install core
+snap install node --classic --channel=14
+
+node -v
 
 # 安装yt-dlp
 pip3 install ffmpeg
 
 # 安装FFmpeg
 pip3 install yt-dlp
+
+yt-dlp -U
+ffmpeg -version
 
 # 执行npm run命令
 npm run
